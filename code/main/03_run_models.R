82f11e6ef863901794486fbfdeb1570084ef8b5a
Emily Nightingale <phpuenig@lshtm.ac.uk>
2021-05-07 13:26:06 +0100

    update priors

--- code/main/03_run_models.R
+++ code/main/03_run_models.R
@@ -32,39 +32,56 @@ dat$n[dat$wk_since_first < 0] <- NA
 # PRIOR SPECIFICATION
 ################################################################################
 
-# Dispersion in RR (n/E) - variance >2x mean
+# Dispersion in RR (n/E) - variance ~2x mean
 var(dat$n/dat$E_wk, na.rm = TRUE)/mean(dat$n/dat$E_wk, na.rm = TRUE)
 
-# SD of RR over time
-dat %>%
-  group_by(w) %>%
-  summarise(n = sum(n, na.rm = TRUE), E = sum(E_wk)) %>%
-  mutate(rate = n/E) %>%
-  pull(rate) %>%
-  var(na.rm = TRUE) %>%
-  sqrt() -> sd_time
+nullmod <- MASS::glm.nb(n ~ IMD_quint + prop_minority + offset(log(E_wk)), dat)
+summary(nullmod)
 
-# SD of RR over space
+res <- residuals(nullmod)
+sd(res)
+
 dat %>%
+  filter(!is.na(n)) %>%
+  mutate(res = res) %>%
+  group_by(wk_since_first) %>%
+  summarise(res = mean(res)) %>%
+  pull(res) %>%
+  var(na.rm = TRUE) %>%
+  sqrt() -> sd_time1
+
+dat %>%
+  filter(!is.na(n)) %>%
+  mutate(res = res) %>%
+  group_by(w) %>%
+  summarise(res = mean(res)) %>%
+  pull(res) %>%
+  var(na.rm = TRUE) %>%
+  sqrt() -> sd_time2
+
+dat %>%
+  filter(!is.na(n)) %>%
+  mutate(res = res) %>%
   group_by(lad19cd) %>%
-  summarise(n = sum(n, na.rm = TRUE), E = unique(E_wk)) %>%
-  mutate(rate = n/E) %>%
-  pull(rate) %>%
+  summarise(res = mean(res)) %>%
+  pull(res) %>%
   var(na.rm = TRUE) %>%
   sqrt() -> sd_space
 
-# => Less variation over space than over time
-
-# Undajusted for other covariates and structure so take these as upper bounds:
-prior.prec.tp <- list(prec = list(prior = "pc.prec",
-                                  param = c(sd_time/0.31, 0.01)))
+# Undajusted for correlation structure so take these as upper bounds:
+prior.prec.tp1 <- list(prec = list(prior = "pc.prec",
+                                  param = c(sd_time1/0.31, 0.01)))
+prior.prec.tp2 <- list(prec = list(prior = "pc.prec",
+                                  param = c(sd_time2/0.31, 0.01)))
 prior.prec.sp <- list(prec = list(prior = "pc.prec",
-                                  param = c(sd_space/0.31, 0.01),
-                                  initial = log(0.000001), fixed = TRUE))
+                                  param = c(sd_space/0.31, 0.01)))
 
 # Sample and plot priors on SD scale
-prior.samp.tp <- inla.pc.rprec(1000,u = sd_time/0.31, alpha = 0.01)
-hist(1/sqrt(prior.samp.tp), breaks = 100)
+prior.samp.tp1 <- inla.pc.rprec(1000, u = sd_time1/0.31, alpha = 0.01)
+hist(1/sqrt(prior.samp.tp1), breaks = 100)
+
+prior.samp.tp2 <- inla.pc.rprec(1000, u = sd_time2/0.31, alpha = 0.01)
+hist(1/sqrt(prior.samp.tp2), breaks = 100)
 
 prior.samp.sp <- inla.pc.rprec(1000,u = sd_space/0.31, alpha = 0.01)
 hist(1/sqrt(prior.samp.sp), breaks = 100)
@@ -78,34 +95,28 @@ f_base <- n ~
   IMD_quint + prop_minority +
   # RW2 on epidemic week 
   f(wk_since_first, model = "rw2",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
+    hyper = list(prec = prior.prec.tp1)) +
   # RW1 on week overall
   f(w, model = "rw1",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) 
+    hyper = list(prec = prior.prec.tp2)) 
 
 ## No spatial effect, geography-dependent RW
 f_base_geog <- n ~ 
   IMD_quint + prop_minority +
   # RW2 on epidemic week, replicated by geography
   f(wk_since_first, model = "rw2",
-    hyper = list(prec = prior.prec.tp),
-    replicate = geog,
-    scale.model = T) +
+    hyper = list(prec = prior.prec.tp1),
+    replicate = geog) +
   f(w, model = "rw1",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) 
+    hyper = list(prec = prior.prec.tp2)) 
 
 ## IID spatial
 f_iid <- n ~ 
   IMD_quint + prop_minority + 
-  f(w, model = "rw1",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
   f(wk_since_first, model = "rw2",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
+    hyper = list(prec = prior.prec.tp1)) +
+  f(w, model = "rw1",
+    hyper = list(prec = prior.prec.tp2)) +
   f(la, model = "iid", 
     constr = T,
     hyper=list(
@@ -115,13 +126,11 @@ f_iid <- n ~
 ## IID spatial with geography-dependent RW
 f_iid_geog <- n ~ 
   IMD_quint + prop_minority + 
-  f(w, model = "rw1",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
   f(wk_since_first, model = "rw2",
-    hyper = list(prec = prior.prec.tp),
-    replicate = geog,
-    scale.model = T) +
+    hyper = list(prec = prior.prec.tp1),
+    replicate = geog) +
+  f(w, model = "rw1",
+    hyper = list(prec = prior.prec.tp2)) +
   f(la, model = "iid", 
     constr = T,
     hyper=list(
@@ -131,14 +140,11 @@ f_iid_geog <- n ~
 ## BYM spatial
 f_bym <- n ~ 
   IMD_quint + prop_minority + 
-  f(w, model = "rw1",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
   f(wk_since_first, model = "rw2",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
+    hyper = list(prec = prior.prec.tp1)) +
+  f(w, model = "rw1",
+    hyper = list(prec = prior.prec.tp2)) +
   f(la, model = "bym2", graph = g,
-    scale.model = T,
     constr = T,
     hyper=list(
       phi =list(param = c(0.5, 2/3)),
@@ -148,15 +154,12 @@ f_bym <- n ~
 ## BYM spatial effect, geography-dependent RW
 f_bym_geog <- n ~ 
   IMD_quint + prop_minority + 
-  f(w, model = "rw1",
-    hyper = list(prec = prior.prec.tp),
-    scale.model = T) +
   f(wk_since_first, model = "rw2",
-    hyper = list(prec = prior.prec.tp),
-    replicate = geog,
-    scale.model = T) +
+    hyper = list(prec = prior.prec.tp1),
+    replicate = geog) +
+  f(w, model = "rw1",
+    hyper = list(prec = prior.prec.tp2)) +
   f(la, model = "bym2", graph = g,
-    scale.model = T,
     constr = T,
     hyper=list(
       phi =list(param = c(0.5, 2/3)),
@@ -170,7 +173,7 @@ fits <- lapply(formulae, fit_mod, dat, expected = expected)
 # Check failed cpo values
 lapply(fits, function(f) summary(f$cpo$failure))
 
-saveRDS(fits, file = here::here("output",sprintf("fits_%s_%s.rds",measure, wave)))
+saveRDS(fits, file = here::here(outdir,sprintf("fits_%s_%s.rds",measure, wave)))
 
 # Just final model
 # fit <- fit_mod(f_bym_geog, dat, expected = expected)
@@ -179,7 +182,7 @@ saveRDS(fits, file = here::here("output",sprintf("fits_%s_%s.rds",measure, wave)
 
 # ## Draw posterior samples ##
 samples <- lapply(fits, inla.posterior.sample,n = nsims)
-saveRDS(samples, file =  here::here("output",sprintf("samples_%s_%s.rds",measure, wave)))
+saveRDS(samples, file =  here::here(outdir,sprintf("samples_%s_%s.rds",measure, wave)))
 
 # Just final model
 # samples <- inla.posterior.sample(fit, n = 1000)
